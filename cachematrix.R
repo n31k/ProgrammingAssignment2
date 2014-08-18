caa175fa4471acd8ba08a59a10534f9ea280781c
N31k <n31k0s@gmail.com>
2014-08-18 20:01:20 +0300

    Added Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are two functions written as a course assignment.  
+## The first function returns a list of appropriate functions
+## of the input, with which a matrix x is hold, and its inverse
+## can be calculated and cached. For usage, consult the respective commments
+## above each function.
 
-## Write a short comment describing this function
+## In order to use this function, assign it to a name in the workspace (e.g. "a").
+## Then "a" is a list of functions. Usage: a <- make CacheMatrix() ;
+## a$set(X), where X is an invertible matrix; this will set the matrix argument to be X
+## a$setinverse() , which will assign the inverted matrix in the right place in the list to
+## hold, accesible with a$getinverse().
 
 makeCacheMatrix <- function(x = matrix()) {
 	minv <- NULL
@@ -15,7 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-## Write a short comment describing this function
+## This function takes an object created by makeCacheMatrix, and applies
+## the appropriate functions in the list so that it uses the cached inverse
+## if available, otherwise computes and caches it. For example: 
+## X <- matrix(runif(100), 10, 10);
+## a <- makeCacheMatrix(X); cacheSolve(a) ; the first time it computes the 
+## inverse from scratch and returns it; but if run again cacheSolve(a), it will 
+## find the cached inverse and return it from memory.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
